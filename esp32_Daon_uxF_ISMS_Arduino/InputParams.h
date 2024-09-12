32caa6126076f1d6be5f24dfc4fa4e08193f56c6
Daon_Sori <smcdidcksthf@naver.com>
2024-09-12 13:44:42 +0900

    아두이노 IDE용 코드 추가; 코드 동일화작업 주의

---
+++ esp32_Daon_uxF_ISMS_Arduino/InputParams.h
@@ -0,0 +1,65 @@
+#ifndef INPUTPARAMS_H
+#define INPUTPARAMS_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "SPIFFS.h"
+
+class InputParams
+{
+public:
+  InputParams(int numOfSensors) : numberOfSensors(numOfSensors), protocol(""), espnow(""), address(""), sensor(""), period(""), mac(""), protocolMode(0), espnowMode(0), modbusAddress(0), sensorType(0), sleepPeriod(0), newMacAddress() {};
+
+  String protocol;
+  String espnow;
+  String address;
+  String sensor;
+  String period;
+  String mac;
+  String addition;
+
+  int protocolMode;
+  int espnowMode;
+  int modbusAddress;
+  int sensorType;
+  int sleepPeriod;
+  uint8_t newMacAddress[6];
+  int additionValue;
+
+  int numberOfSensors;
+
+  const char *PARAM_INPUT_1 = "protocol";
+  const char *PARAM_INPUT_2 = "espnow";
+  const char *PARAM_INPUT_3 = "address";
+  const char *PARAM_INPUT_4 = "sensor";
+  const char *PARAM_INPUT_5 = "period";
+  const char *PARAM_INPUT_6 = "mac";
+  const char *PARAM_INPUT_7 = "addition";
+
+  const char *protocolPath = "/protocol.txt";
+  const char *espnowPath = "/espnow.txt";
+  const char *addressPath = "/address.txt";
+  const char *sensorPath = "/sensor.txt";
+  const char *periodPath = "/period.txt";
+  const char *macPath = "/mac.txt";
+  const char *additionPath = "/addition.txt";
+
+  void setParam(AsyncWebParameter *p);
+  bool checkSettingValues();
+  void showSettingValues();
+  void writeToFile();
+  void readFromFile();
+  bool isWiredCommunicationMode();
+  bool isWirelessCommunicationMode();
+  bool isRs485Mode();
+  bool isSensingMode();
+  void parseMacString();
+};
+
+void initSPIFFS();
+void writeFile(fs::FS &fs, const char *path, const char *message);
+String readFile(fs::FS &fs, const char *path);
+
+#endif
